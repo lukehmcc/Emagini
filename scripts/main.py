0b11d9beffb45140b663396fc5a1acac2b51ab40
lukehmcc <lukehmcc@gmail.com>
2020-03-05 19:31:07 -0500

    .1.2

--- scripts/main.py
+++ scripts/main.py
@@ -6,11 +6,16 @@ on the Sia network
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from android.permissions import request_permissions, Permission
+from android.storage import primary_external_storage_path
 
 ##Initaites the basic app
 class BasicApp(App):
     def build(self):
-        return Label(text="Hello world")
+        request_permissions([Permission.WRITE_EXTERNAL_STORAGE,
+                     Permission.READ_EXTERNAL_STORAGE])
+        self.SD_CARD = primary_external_storage_path()
+        return 
 
 
 ##Initiates if this is the running file
