a2850dae96a4de2ab58ebec5e645e84f4b6352d1
lukehmcc <lukehmcc@gmail.com>
2020-03-08 12:59:26 -0400

    .1.3

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,7 @@ Author: Jim Grid & Will Brent
 Purpose: A basic google photo's comptetitor built
 on the Sia network
 """
+__version__ = '.1.2'
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -11,30 +12,29 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
 import glob
-#from android.permissions import request_permissions, Permission
-#from android.storage import primary_external_storage_path
+import numpy
+from android.permissions import request_permissions, Permission
+from android.storage import primary_external_storage_path
 
+class ImageMaker(Image):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print(self.source)
 
 ##Creates the basic gallery screen
 class Gallery(GridLayout):
     def __init__(self, **kwargs):
         super(Gallery, self).__init__(**kwargs)
-        thumb.bind(on_touch_down=self.callback)    # I tried on_touch property but does not work with images only buttons
-            self.add_widget(thumb)
+        thumb.bind(on_touch_down=self.callback)
+        self.add_widget(thumb)
 
     def callback(self, obj, touch):
-        # This should print only the clicked image source. 
-        # (but instead is printing all images sources at once)
-        print obj.sourceself.SD_CARD = primary_external_storage_path()
+        SD_CARD = primary_external_storage_path()
         images = glob.glob(self.SD_CARD) 
-        self.cols = 4
-        for img in self.SD_CARD:
-            thumb = Image(source=img)
-            thumb.bind(on_touch_down=self.callback)
-            self.add_widget(thumb)
-
-    def callback(self, obj, touch):
-        print(obj.source)
+        self.cols = 3
+        for img in images:
+            thumb = ImageMaker(source = img)
+            self.add_widget(thumb)   
 
 ##Initaites the basic app
 class BasicApp(App):
