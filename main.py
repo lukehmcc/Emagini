e3d1de608ebdef62341688d850c1d1db783dcce3
lukehmcc <58121030+lukehmcc@users.noreply.github.com>
2020-03-08 12:51:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+Author: Jim Grid & Will Brent
+Purpose: A basic google photo's comptetitor built
+on the Sia network
+"""
+__version__ = '.1.2'
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
+import glob
+import numpy
+from android.permissions import request_permissions, Permission
+from android.storage import primary_external_storage_path
+
+class ImageMaker(Image):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            print(self.source)
+
+##Creates the basic gallery screen
+class Gallery(GridLayout):
+    def __init__(self, **kwargs):
+        super(Gallery, self).__init__(**kwargs)
+        thumb.bind(on_touch_down=self.callback)
+        self.add_widget(thumb)
+
+    def callback(self, obj, touch):
+        SD_CARD = primary_external_storage_path()
+        images = glob.glob(self.SD_CARD) 
+        self.cols = 3
+        for img in images:
+            thumb = ImageMaker(source = img)
+            self.add_widget(thumb)   
+
+##Initaites the basic app
+class BasicApp(App):
+    def build(self):
+        request_permissions([Permission.WRITE_EXTERNAL_STORAGE,
+                     Permission.READ_EXTERNAL_STORAGE])
+        return Gallery() 
+
+
+##Initiates if this is the running file
+if __name__ == "__main__":
+    BasicApp = BasicApp()
+    BasicApp.run()
