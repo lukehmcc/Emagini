06f9fdb99ffa162a127effa844d67e07efdb8193
lukehmcc <lukehmcc@gmail.com>
2020-03-22 22:01:22 -0400

    that looks terrible... still working on it

--- emaginiReact/App.js
+++ emaginiReact/App.js
@@ -6,32 +6,45 @@ import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
 } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class emagini extends Component {
   render() {
     return (
-      <View style={helloStyle.container}>
-        <Text style={helloStyle.instructions}>Hello, you have reached an automated voice messaging system, please dial 9 for more options...</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.thiccText}>Hello, you have reached an automated voice messaging system, please dial 9 for more options...</Text>
+        </View>
+        <View style={styles.body}>
+          <Text style={styles.thinText}>Good morning</Text>
+        </View>
       </View>
     );
   }
 }
-
-const helloStyle = StyleSheet.create({
+const styles = StyleSheet.create({
   container: { 
               flex: 1, 
               justifyContent: "center", 
               alignItems: "center", 
-              backgroundColor: '#32a852' 
+              backgroundColor: '#FFFFFF'
   },
-  instructions: {
+  header:{
+              textAlign: 'center'
+  },
+  thiccText: {
               textAlign: 'center',
-		          color: '#333333',
+    		      color: '#333333',
 		          marginBottom: 5
-  }
+  },
+  body: { 
+              backgroundColor: '#00c0f0',
+              width: '100%'
+  },
+  thinText: {
+              textAlign: 'center'
+  },
 });
