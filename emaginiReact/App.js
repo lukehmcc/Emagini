4b3782fcb4e3f6e97bd382e13e9082dafd2ad07d
lukehmcc <lukehmcc@gmail.com>
2020-04-05 15:36:15 -0400

    hey it works, but you should probably fix the whole hting dummy

--- emaginiReact/App.js
+++ emaginiReact/App.js
@@ -1,7 +1,6 @@
-/**
- * This is an app created by a literal god
+/*
+ This is an app created by a literal god
  */
-
 import React, { Component } from 'react';
 import {
   SafeAreaView,
@@ -11,6 +10,8 @@ import {
   StatusBar,
   Image,
   ImageBackground,
+  Button,
+  Animated,
 } from 'react-native';
 import { AuthSession } from 'expo';
 import Svg, {
@@ -27,42 +28,57 @@ import Svg, {
     Use,
     Symbol,
     Defs,
-    LinearGradient,
     RadialGradient,
     Stop,
     ClipPath,
     Pattern,
     Mask,
   } from 'react-native-svg';
+  import { LinearGradient } from 'expo-linear-gradient'
+
 
 const river = {uri:"https://emaginiphotos.s3.filebase.com/river.jpg"}
 const mountains = {uri:"https://emaginiphotos.s3.filebase.com/mountains.jpg"}
 
-export default class emagini extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <ImageBackground source={mountains} style={styles.mountains}>
+function Emagini() {
+
+    
+
+    return (
+        <View style={styles.container}>
+                <LinearGradient colors={['#FFFFFF', '#8cacc9c7']} style={styles.gradient}>
                     <View style={styles.spacer}></View>
                     <Text style={styles.title}> Welcome User </Text>
-                    <Text style={styles.title2}> To <Text style={styles.title3}> Emagini </Text> </Text>
-                    <View style={styles.triangleCorner}></View>
-                    <View style={styles.textbox}>
-                        <View style={styles.textbox2}>
-                           <Text style={styles.mainText1}>
-                               Emagini is the photo sharing app that respects your security 
-                           </Text>
-                           
+                    <View style={styles.sameLine}>
+                        <Text style={styles.title2}> To </Text>   
+                        <View style={styles.emaginiBox}>
+                            <Text style={styles.title3}>
+                                Emagini
+                            </Text> 
                         </View>
                     </View>
-                </ImageBackground>
+
+                    <View style={styles.polariod}/>
+                    <View style={styles.lineBelowPols}/>
+
+                    <View>
+                        <Text style={styles.words}>
+                            A photo backup Service that respects your privacy…
+                        </Text>
+                        <Text style={styles.words}>
+                        A novel idea right?
+                        </Text>
+                    </View>
+                </LinearGradient>
             </View>
-        );
-    }
+    )
 }
 const styles = StyleSheet.create({
     container: {
  
+    },
+    gradient: { 
+        height: '100%',
     },
     spacer: { 
         padding: 20,
@@ -71,7 +87,7 @@ const styles = StyleSheet.create({
         color: "#363636",
         fontSize: 40,
         fontWeight: "bold",
-        alignSelf: 'flex-end', 
+        alignSelf: 'center', 
         
     },
     title2: {
@@ -79,50 +95,42 @@ const styles = StyleSheet.create({
         fontSize: 40,
         fontWeight: "bold",
         alignSelf: 'flex-end',
-        paddingRight: 20,        
+        paddingRight: -20,
     },
     title3: {
         color: 'white',
         fontSize: 40,
         fontWeight: "bold",
-        alignSelf: 'flex-end',
-        paddingRight: 10,   
+        alignSelf: 'center',
     },
-    mainText1: { 
+    sameLine: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+    },
+    emaginiBox: {
+        backgroundColor: '#8cacc9',
+        width: '46%',
+        height: '100%',
+        borderRadius: 20,
+    },
+    words: {
+        alignSelf: 'center',
         fontSize: 20,
-        color: '#FFFFFF',
+        color: '#2e2e2e',
         padding: 10,
     },
-    mountains:{
-        height:'100%',
+    lineBelowPols: {
+        borderBottomColor: 'darkgrey',
+        borderBottomWidth: 2,
+        padding: 10,
     },
+    polariod: {
+        width: '10%',
+        height: '10%',
+        backgroundColor: 'black',
+        borderBottomColor: 'white',
+    }
 
-    triangleCorner: {
-        width: 0,
-        height: 0,
-        backgroundColor: 'transparent',
-        borderStyle: 'solid',
-        borderTopWidth: 0,
-        borderRightWidth: 600,
-        borderBottomWidth: 90,
-        borderLeftWidth: 0,
-        borderTopColor: 'transparent',
-        borderRightColor: 'transparent',
-        borderLeftColor: 'transparent',
-        borderBottomColor: '#40404080',
-        alignSelf: 'flex-end',
-        marginTop: -40,
-    },    
-    textbox: {
-        backgroundColor: '#40404080',
-        width: '100%',
-        height: '100%',
-    },
-    textbox2: {
-        backgroundColor: '#40404050',
-        width: '90%',
-        height: '65%',
-        alignSelf: 'center',
-        marginTop: 20,
-    },
 });  
+
+export default Emagini;
